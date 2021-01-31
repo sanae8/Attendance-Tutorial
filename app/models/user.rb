f517f09bd011e6931a0ff743c104b2c8093208d9
sanae8 <kinnobuta77@gmail.com>
2021-01-31 10:07:02 +0000

    9.1所属カラム

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,8 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 100 },
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: true    
+                    uniqueness: true
+  validates :department, length: { in: 2..30 }, allow_blank: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
@@ -28,16 +29,16 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
 
- # トークンがダイジェストと一致すればtrueを返します。
-  def authenticated?(remember_token)
-    # ダイジェストが存在しない場合はfalseを返して終了します。
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  # 永続セッションのためハッシュ化したトークンをデータベースに記憶します。
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
-
 
   # トークンがダイジェストと一致すればtrueを返します。
   def authenticated?(remember_token)
+    # ダイジェストが存在しない場合はfalseを返して終了します。
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
